0fef031b8903dbdaa009ff9981e9db61207d0e90
prontokan <kan@prontomarketing.com>
2014-05-28 20:34:51 +0700

    remove cd command (not necessary) :bear:

--- bypronto-setup.sh
+++ bypronto-setup.sh
@@ -5,4 +5,3 @@ cp vvv-nginx.conf vvv-bypronto/www/
 cp wp-tests-config.php vvv-bypronto/www/
 git clone git@github.com:prontodev/bypronto.git vvv-bypronto/www/bypronto
 mv vvv-bypronto ..
-cd ../vvv-bypronto/
