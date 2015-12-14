54e2546a3ed049d4fab1f9dea4a179e910e9f94d
mrteera <webmaster@mrteera.com>
2015-12-14 15:38:36 +0700

    Cannot remove wordpress-develop in vvv-init.sh
    We'll remove it later in another script

--- vvv-init.sh
+++ vvv-init.sh
@@ -34,13 +34,11 @@ echo "Creating Bypronto database for test (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS bypronto_test"
 mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON bypronto_test.* TO wp@localhost IDENTIFIED BY 'wp';"
 
-rm -rf /srv/www/wordpress-develop/
-ln -sf /srv/www/bypronto /srv/www/wordpress-develop
-cp /srv/www/wp-tests-config.php /srv/www/wordpress-develop/web/
-cp /srv/www/bootstrap.php /srv/www/wordpress-develop/tests/phpunit/includes/
+cp /srv/www/wp-tests-config.php /srv/www/bypronto/web/
+cp /srv/www/bootstrap.php /srv/www/bypronto/tests/phpunit/includes/
 
 # Generate the wp-config file
-wp core config --dbname="bypronto" --dbuser=root --dbpass=root --dbhost="localhost" --allow-root --path=/srv/www/wordpress-develop/web/ --extra-php <<PHP
+wp core config --dbname="bypronto" --dbuser=root --dbpass=root --dbhost="localhost" --allow-root --path=/srv/www/bypronto/web/ --extra-php <<PHP
 \$memcached_servers = array( 'default' => array( '127.0.0.1:11211' ) );
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
@@ -49,23 +47,23 @@ define( 'DEVBAR_NOTIFY', true );
 PHP
 
 # Install multisite
-wp core multisite-install --url=local.bypronto.dev --subdomains --title="Phoenix Test" --admin_user=admin --admin_password=password --admin_email=admin@pronto.com --allow-root --path=/srv/www/wordpress-develop/web/
-wp theme activate phoenix-child --allow-root --path=/srv/www/wordpress-develop/web/
+wp core multisite-install --url=local.bypronto.dev --subdomains --title="Phoenix Test" --admin_user=admin --admin_password=password --admin_email=admin@pronto.com --allow-root --path=/srv/www/bypronto/web/
+wp theme activate phoenix-child --allow-root --path=/srv/www/bypronto/web/
 
 # Install plugins
-wp plugin install debug-bar --activate --allow-root --path=/srv/www/wordpress-develop/web
-wp plugin install log-deprecated-notices --activate --allow-root --path=/srv/www/wordpress-develop/web/
+wp plugin install debug-bar --activate --allow-root --path=/srv/www/bypronto/web
+wp plugin install log-deprecated-notices --activate --allow-root --path=/srv/www/bypronto/web/
 
 # Enable Object Cache on Bypronto.
 echo "Enable Object Cache on Bypronto"
-ln -s /srv/www/wordpress-develop/conf/object-cache.php /srv/www/wordpress-develop/web/wp-content/
+ln -s /srv/www/bypronto/conf/object-cache.php /srv/www/bypronto/web/wp-content/
 
 # Install WP Mock
 echo "Install WP Mock on Bypronto"
 composer require --dev 10up/wp_mock:dev-master
-mv composer.json /srv/www/wordpress-develop/web/
-mv composer.lock /srv/www/wordpress-develop/web/
-mv vendor /srv/www/wordpress-develop/web/
+mv composer.json /srv/www/bypronto/web/
+mv composer.lock /srv/www/bypronto/web/
+mv vendor /srv/www/bypronto/web/
 
 ## The Vagrant site setup script will restart Nginx for us
 
